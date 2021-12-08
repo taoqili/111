f5c8225065025b84fbb7f46236921b7c00fd7202
taoqili <taoqili@kuaishou.com>
2021-12-08 15:02:14 +0800

    test

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -9,7 +9,7 @@ import {stylesHeader, stylesImage} from './app.module.scss';
 const LazyStrawberryIcon = lazy(() => import('./strawberry'));
 export const App = (): React.ReactElement => (
     <div className={stylesContainer}>
-        <div className={stylesHeader}>It works</div>
+        <div className={stylesHeader}>It works!!</div>
         <Suspense fallback={'loading...'}>
             <LazyStrawberryIcon className={stylesImage} />
         </Suspense>
