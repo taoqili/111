18821677e4d5bc30eb9a7c09d9f6857103421307
陶启立 <taoqili@126.com>
2022-06-12 22:29:18 +0800

    Create app.tsx

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -10,7 +10,7 @@ const LazyStrawberryIcon = lazy(() => import('./strawberry'));
 export const App = (): React.ReactElement => (
     <div className={stylesContainer}>
         <div className={stylesHeader}>It works!</div>
-        <Suspense fallback={'loading...'}>
+        <Suspense fallback={'loading.....'}>
             <LazyStrawberryIcon className={stylesImage} />
         </Suspense>
     </div>
